f8c1ab7b2c3ae32ca29d74f3fcb7fd8ad387e91c
rebeccawaweru <wawerur95@gmail.com>
2024-07-06 18:12:22 +0300

    setting up express server, creating database connection with postgres

---
+++ server/src/index.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+const app = express();
+
+app.use(express.json());
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
