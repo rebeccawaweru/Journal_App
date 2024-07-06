f7602e3f70c4da4fd9954f946a9056c4bbc5d2a9
rebeccawaweru <wawerur95@gmail.com>
2024-07-06 21:23:49 +0300

    setting up navigation, creating authentication screens, setting up App entry point

---
+++ mobile/src/screens/LoginScreen.tsx
@@ -0,0 +1,29 @@
+import React, {useState} from "react";
+import { View, Text, TextInput, Button} from 'react-native';
+import client from '../client/api'
+const LoginScreen = ({ navigation }) => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleLogin = async () => {
+       try {
+        const response = await client.post('/auth/login',{username,password});
+        if (response.data.token) {
+           navigation.navigate('Home')
+        }
+       } catch (error) {
+           console.log(error)
+       }
+    };
+    return (
+        <View>
+            <Text>Login</Text>
+            <TextInput placeholder="Username" value={username} onChangeText={setUsername} />
+            <TextInput placeholder="Password" value={password} onChangeText={setPassword} secureTextEntry />
+            <Button title="Login" onPress={handleLogin}/>
+            <Button title="Register" onPress={()=>navigation.navigate('Register')}/>
+        </View>
+    )
+}
+
+export default LoginScreen;
---
+++ mobile/src/screens/RegisterScreen.tsx
@@ -0,0 +1,30 @@
+import React, {useState} from "react";
+import { View, Text, TextInput, Button } from "react-native";
+import axios from 'axios'
+import client from '../client/api'
+const RegisterScreen = ({ navigation }) => {
+    const [username,setUsername] = useState('')
+    const [password,setPassword] = useState('')
+
+    const handleRegister = async () => {
+        try {
+            const response = await client.post('/auth/register', {username,password})
+            if (response.data.success) {
+                navigation.navigate('Login');
+            }
+        } catch (error) {
+            console.error(error)
+        }
+    }
+    return (
+        <View>
+            <Text>Register</Text>
+            <TextInput placeholder="Username" value={username} onChangeText={setUsername} />
+            <TextInput placeholder="Password" value={password} onChangeText={setPassword} secureTextEntry />
+            <Button title="Register" onPress={handleRegister}/>
+            <Button title="Login" onPress={()=>navigation.navigate('Login')}/>
+        </View>
+    )
+}
+
+export default RegisterScreen;
