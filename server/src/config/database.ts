83667104c307740b6525b9b94cafbc51036af5bb
rebeccawaweru <wawerur95@gmail.com>
2024-07-09 02:03:02 +0300

    updating database configuration variables

--- server/src/config/database.ts
+++ server/src/config/database.ts
@@ -5,10 +5,10 @@ const username = process.env.DB_USERNAME as string;
 const password = process.env.DB_PASSWORD as string;
 const host = process.env.DB_HOST as string;
 const port = parseInt(process.env.PORT as string, 10);
-const sequelize = new Sequelize('journals','postgres','becca254', {
-    host:'localhost',
+const sequelize = new Sequelize(db,username,password, {
+    host:host,
     dialect:'postgres',
-    port:5000
+    port:port
 });
 
 export default sequelize
